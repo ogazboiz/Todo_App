3fbcf42ff86ecc5995a467cfc4148658717c17b7
Akpolo Prince <ogazboizakpolo@gmail.com>
2022-11-15 12:01:09 +0100

    under construction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ogazboiz@localhos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
@@ -22,30 +23,32 @@ def index():
     todo_list = Todo.query.all()
     return render_template('index.html', todo_list=todo_list)
 
-@app.route("/add", methods=["POST"])
-def add():
-    #add new item
-    title = request.form.get("title")
+@app.route("/todos/create", methods=['POST'])
+def create_todo():
+    #create new item
+    body={}
+    title = request.get_json()['title']
     new_todo = Todo(title=title, complete=False)
+    body['title'] = new_todo.title
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("index"))
+    return jsonify(body)
 
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
-    #add new item
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("index"))
+# @app.route("/update/<int:todo_id>")
+# def update(todo_id):
+#     #add new item
+#     todo = Todo.query.filter_by(id=todo_id).first()
+#     todo.complete = not todo.complete
+#     db.session.commit()
+#     return redirect(url_for("index"))
 
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
-    #delete item
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+# @app.route("/delete/<int:todo_id>")
+# def delete(todo_id):
+#     #delete item
+#     todo = Todo.query.filter_by(id=todo_id).first()
+#     db.session.delete(todo)
+#     db.session.commit()
+#     return redirect(url_for("index"))
 
 if __name__ == "__main__":
     db.create_all()
