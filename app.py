dd17c4a6d783dae0ec46eb34bfad7551ea2c92b4
Akpolo Prince <ogazboizakpolo@gmail.com>
2022-11-17 01:40:01 +0100

    completed the index.html and app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -10,18 +11,32 @@ app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ogazboiz@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+    completed = db.Column(db.Boolean, default = False)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    completed = db.Column(db.Boolean)
-
-
+    completed = db.Column(db.Boolean, default = False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
 @app.route('/')
 def index():
-    todo_list = Todo.query.order_by('id').all()
-    return render_template('index.html', todo_list=todo_list)
+    return redirect(url_for('get_list_todos', list_id=1))
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    
+    todo_list = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    return render_template('index.html',lists = TodoList.query.all(),active_list=TodoList.query.get(list_id), todo_list=todo_list)
 
 @app.route("/todos/create", methods=['POST'])
 def create_todo():
@@ -30,10 +45,14 @@ def create_todo():
     try:
         body={}
         title = request.get_json()['title']
-        new_todo = Todo(title=title, completed=False)
-        body['title'] = new_todo.title
+        list_id = request.get_json()['list_id']
+        new_todo = Todo(title=title, completed=False, list_id=list_id)
         db.session.add(new_todo)
         db.session.commit()
+        body['title'] = new_todo.title
+        body['id'] = new_todo.id
+        body['completed'] = new_todo.completed
+        
         
     except:
         error = True
@@ -86,8 +105,73 @@ def delete_todo(todo_id):
     db.session.close()
   return jsonify({ 'success': True })
 
+@app.route('/lists/create', methods=['POST'])
+def create_list():
+    error = False
+    body = {}
+    try:
+        name = request.get_json()['name']
+        todolist = TodoList(name=name)
+        db.session.add(todolist)
+        db.session.commit()
+        body['id'] = todolist.id
+        body['name'] = todolist.name
+    except:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
+
+@app.route('/lists/<list_id>/delete', methods=['DELETE'])
+def delete_list(list_id):
+    error = False
+    try:
+        list = TodoList.query.get(list_id)
+        for todo in list.todos:
+            db.session.delete(todo)
+
+        db.session.delete(list)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify({'success': True})
+
+
+@app.route('/lists/<list_id>/set-completed', methods=['POST'])
+def set_completed_list(list_id):
+    error = False
+
+    try:
+        list = TodoList.query.get(list_id)
+        completed = request.get_json()['completed']
+        for todo in list.todos:
+            todo.completed = True
+        list.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+
+        error = True
+    finally:
+        db.session.close()
+
+    if error:
+        abort(500)
+    else:
+        return '', 200
 
 if __name__ == "__main__":
-    db.create_all()
+    # db.create_all()
     app.run(host="0.0.0.0", debug=True)
     
